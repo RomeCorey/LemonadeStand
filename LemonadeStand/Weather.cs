83ffcb7e597635ae1a68b4abf7e44e1d51289b9c
Corey Rome <coreyrome2@gmail.com>
2017-11-27 10:59:36 -0600

    Functions for weather and temp added.

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -8,5 +8,62 @@ namespace LemonadeStand
 {
     class Weather
     {
+        public enum TempChoice { Fifties, Sixties, Seventies, Eighties, Nineties, }
+
+        Random random = new Random();
+
+        public string getWeather()
+        {
+
+            int number = random.Next(1, 5);
+            if (number == 1)
+            {
+                return "Rain";
+            }
+            if (number == 2)
+            {
+                return "Overcast";
+            }
+            if (number == 3)
+            {
+                return "Cloudy";
+            }
+            if (number == 4)
+            {
+                return "Sunny";
+            }
+            return "";
+        }
+
+        public string getTemperature()
+        {
+
+            int number = random.Next(1, 7);
+            if (number == 1)
+            {
+                return "50's";
+            }
+            if (number == 2)
+            {
+                return "60's";
+            }
+            if (number == 3)
+            {
+                return "70's";
+            }
+            if (number == 4)
+            {
+                return "80's";
+            }
+            if (number == 5)
+            {
+                return "90's";
+            }
+            if (number == 6)
+            {
+                return "100's";
+            }
+            return "";
+        }
     }
 }
