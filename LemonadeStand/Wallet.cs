45dfd2cd6429d0ef41f80daa24ff05ece5a4a606
Corey Rome <coreyrome2@gmail.com>
2017-11-29 10:01:45 -0600

    wallet updated

--- LemonadeStand/Wallet.cs
+++ LemonadeStand/Wallet.cs
@@ -8,5 +8,27 @@ namespace LemonadeStand
 {
     class Wallet
     {
+        public double Money = 20.00;
+        public bool Bankrupt;
+
+
+        public double GetCurrentMoney()
+        {
+            return Money;
+        }
+        public bool CheckWallet(ItemCost)
+        {
+            if (ItemCost > Money)
+                Console.WriteLine("You do not have enough money!");
+            return Bankrupt;
+        }
+        public double BuyItem()
+        {
+            if (!Bankrupt)
+            {
+                Money = ItemCost - Money;                
+                return Money;
+            }
+        }
     }
 }
