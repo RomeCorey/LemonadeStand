b8369e2035beb353255b771bf85c2443fea6263a
Corey Rome <coreyrome2@gmail.com>
2017-11-30 20:49:08 -0600

    Recipies class updated

--- LemonadeStand/Recipe.cs
+++ LemonadeStand/Recipe.cs
@@ -8,6 +8,12 @@ namespace LemonadeStand
 {
     class Recipe
     {        
+        public int LemonsPerPitcher;
+        public int IcePerPitcher;
+        public int SugarPerPitcher;
+        public int CupsPerPitcher;
+        public int PitcherQuantity;
+
         public int GetLemonsPerPitcher()
         {            
             Console.WriteLine("How many lemons do you want in each pitcher?");
@@ -30,6 +36,44 @@ namespace LemonadeStand
             string UserInput = Console.ReadLine();
             int SugarPerPitcher = Int32.Parse(UserInput);
             return SugarPerPitcher;
+        }
+
+        public int GetPitcherQuantity()
+        {
+            Console.WriteLine("How many pitchers of lemonade would you like to make? Each pitcher makes 10 cups of lemonade!");
+            string UserInput = Console.ReadLine();
+            int PitcherQuantity = Int32.Parse(UserInput);
+            return PitcherQuantity;
+        }
+
+        public int GetDaysWorthOfLemons()
+        {
+            return LemonsPerPitcher = LemonsPerPitcher * PitcherQuantity;           
+        }
+
+        public int GetDaysWorthOfIce()
+        {
+            return IcePerPitcher = IcePerPitcher * PitcherQuantity;
+        }
+
+        public int GetDaysWorthOfSugar()
+        {
+            return SugarPerPitcher = SugarPerPitcher * PitcherQuantity;
+        }
+
+        public int GetDaysWorthOfCups()
+        {
+            return CupsPerPitcher = CupsPerPitcher * PitcherQuantity;
+        }
+
+
+
+        public int RemoveInventoryForPitchers()
+        {
+            InventoryLemons =- LemonsPerPitcher;
+            InventoryIce =- IcePerPitcher;
+            InventorySugar =- SugarPerPitcher;
+            InvnetoryCups = -10;
         }
     }
 }
